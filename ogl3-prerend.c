0a38c9ee0367c1bae73e2ef2fa3c95f4aa2aad5d
Kaleb Elwert <kelwert@mtu.edu>
2014-11-08 12:05:23 -0800

    Replace GL_CLAMP with GL_CLAMP_TO_EDGE in ogl3-prerend

--- ogl3-prerend.c
+++ ogl3-prerend.c
@@ -297,8 +297,8 @@ int main(int argc, char** argv)
 	glActiveTexture(GL_TEXTURE0);
 	glGenTextures(1, &prerenderTexID);
 	glBindTexture(GL_TEXTURE_2D, prerenderTexID);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
