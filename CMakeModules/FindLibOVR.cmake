2122dec40c6513a8bb1dfca0d1d688a48230ecc5
Scott Kuhl <kuhl@mtu.edu>
2015-02-16 13:26:25 -0500

    fix FindLibOVR

--- CMakeModules/FindLibOVR.cmake
+++ CMakeModules/FindLibOVR.cmake
@@ -1,13 +1,7 @@
 include(FindPackageHandleStandardArgs)
 
-find_library(LibOVR_LIBRARIES ovr)
-set(LibOVR_LIBRARIES ${LibOVR_LIBOVRA})
-
-find_path(LibOVR_INCLUDE OVR.h)
-set(LibOVR_INCLUDE_DIRS ${LibOVR_INCLUDE})
-
-find_path(LibOVR_SRC OVR_CAPI.h)
-set(LibOVR_INCLUDE_DIRS ${LibOVR_INCLUDE_DIRS} ${LibOVR_SRC})
+find_library(LibOVR_LIBRARIES libovr.a)
+find_path(LibOVR_INCLUDE_DIRS OVR.h OVR_CAPI.h)
 
 if(LibOVR_SOURCE)
 	find_package_handle_standard_args(LibOVR DEFAULT_MSG
