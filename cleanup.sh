49da332d9811d2367f512e6e2145d3f62052414b
Scott Kuhl <kuhl@mtu.edu>
2015-04-01 12:52:10 -0400

    improve cleanup script so it runs properly regardless of current working directory

--- cleanup.sh
+++ cleanup.sh
@@ -2,9 +2,11 @@
 
 function cleandir()
 {
+	echo
+	echo "=== Cleaning ${1}"
 	# If makefile still exists, try "make clean"
 	if [[ -e Makefile ]]; then
-		make -C "${1}" clean
+		make --quiet -C "${1}" clean
 	fi
 	rm -vrf "${1}/CMakeFiles"
 	rm -vf  "${1}/CMakeCache.txt"
@@ -15,25 +17,29 @@ function cleandir()
 	rm -vf *~ \#*\#
 }
 
-# Clean the current directory
-cleandir .
+# Get the directory that this script is in (might not be the same as
+# the current working directory).
+THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Clean the directory the script is in.
+cleandir ${THIS_DIR}
 for D in *; do # For each file and directory
     if [ -d "${D}" ]; then # If it is a directory
 		if [[ -e "${D}/CMakeLists.txt" ]]; then # if CMakeLists file is present
-			cleandir "${D}"
+			cleandir "${THIS_DIR}/${D}"
 		fi
     fi
 done
 
-rm -vrf doxygen-docs
-rm -vrf bin/*.frag bin/*.vert bin/*libOVR*.so*
-rm -vf *.exe
+rm -vrf "${THIS_DIR}/doxygen-docs"
+rm -vrf "${THIS_DIR}/bin/*.frag" "${THIS_DIR}/bin/*.vert" "${THIS_DIR}/bin/*libOVR*.so*"
+rm -vf "${THIS_DIR}/*.exe"
 
 
-if [[ -x .git && -x /usr/bin/git ]]; then
+if [[ -x "${THIS_DIR}/.git" && -x /usr/bin/git ]]; then
 	echo
 	echo
 	echo "Consider removing the following files because they are not in the git repo and are not ignored:"
 	echo
-	git ls-files --others --exclude-standard
+	git -C "${THIS_DIR}" ls-files --others --exclude-standard
 fi
