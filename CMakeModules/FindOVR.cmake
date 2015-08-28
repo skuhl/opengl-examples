79a960be0ddd3a70e26afa08e24a816d5160a7b6
Scott Kuhl <kuhl@mtu.edu>
2015-08-28 15:50:59 -0400

    update findovr script

--- CMakeModules/FindOVR.cmake
+++ CMakeModules/FindOVR.cmake
@@ -1,14 +1,25 @@
 include(FindPackageHandleStandardArgs)
 
 # Rekhi Linux lab
-set(CMAKE_LIBRARY_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.4.4/LibOVR/Lib/Linux/Release/x86_64" ${CMAKE_LIBRARY_PATH})
-set(CMAKE_INCLUDE_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.4.4/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
-set(CMAKE_INCLUDE_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.4.4/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
-# Installed in this directory's parent directory (paths lower will be search first)
+#set(CMAKE_LIBRARY_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.4.4/LibOVR/Lib/Linux/Release/x86_64" ${CMAKE_LIBRARY_PATH})
+#set(CMAKE_INCLUDE_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.4.4/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
+#set(CMAKE_INCLUDE_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.4.4/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
+set(CMAKE_LIBRARY_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Debug" ${CMAKE_LIBRARY_PATH})
+set(CMAKE_LIBRARY_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Release" ${CMAKE_LIBRARY_PATH})
+set(CMAKE_INCLUDE_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
+set(CMAKE_INCLUDE_PATH "/home/campus11/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
+
+# Kuhl's machines
+set(CMAKE_LIBRARY_PATH "/home/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Debug" ${CMAKE_LIBRARY_PATH})
+set(CMAKE_LIBRARY_PATH "/home/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Release" ${CMAKE_LIBRARY_PATH})
+set(CMAKE_INCLUDE_PATH "/home/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
+set(CMAKE_INCLUDE_PATH "/home/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
+
+
+# Allow folder to be stored alongside the opengl-examples folder.
 #set(CMAKE_LIBRARY_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.4.4/LibOVR/Lib/Linux/Release/x86_64" ${CMAKE_LIBRARY_PATH})
 #set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.4.4/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
 #set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.4.4/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
-
 set(CMAKE_LIBRARY_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Debug" ${CMAKE_LIBRARY_PATH})
 set(CMAKE_LIBRARY_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Release" ${CMAKE_LIBRARY_PATH})
 set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.5.0.1/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
@@ -16,13 +27,8 @@ set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.5.0.1/Lib
 
 
 
-# An alternative directory name that OVR could be installed in (same style as: https://github.com/jherico/OculusSDK )
-set(CMAKE_LIBRARY_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../OculusSDK/LibOVR/Lib/Linux/Release/x86_64" ${CMAKE_LIBRARY_PATH})
-set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../OculusSDK/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
-set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../OculusSDK/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
-
-
 # library was named libovr.a in 0.4.4 and libOVR.a in 0.5.0.1
+set(CMAKE_LIBRARY_PATH "/usr/lib64" ${CMAKE_LIBRARY_PATH})
 find_library(OVR_LIBRARIES libOVR.a libovr.a)
 find_library(OVR_LIBRARY_SO libOVRRT64_0.so.5)
 find_path(OVR_INCLUDE_DIR_LIB OVR.h)
