f7e4c1bc4c1a0df600403ed22e02678a1d004185
Scott Kuhl <kuhl@mtu.edu>
2015-08-28 16:26:07 -0400

    Removing udev from findovr fixes a compilation issue in the Rekhi labs and without breaking other machines

--- CMakeModules/FindOVR.cmake
+++ CMakeModules/FindOVR.cmake
@@ -28,25 +28,24 @@ set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.5.0.1/Lib
 
 
 # library was named libovr.a in 0.4.4 and libOVR.a in 0.5.0.1
-set(CMAKE_LIBRARY_PATH "/usr/lib64" ${CMAKE_LIBRARY_PATH})
 find_library(OVR_LIBRARIES libOVR.a libovr.a)
 find_library(OVR_LIBRARY_SO libOVRRT64_0.so.5)
 find_path(OVR_INCLUDE_DIR_LIB OVR.h)
 find_path(OVR_INCLUDE_DIR_SRC OVR_CAPI_GL.h)
 
-find_library(OVR_udev udev)
+# find_library(OVR_udev udev)
 find_library(OVR_pthread pthread)
 find_library(OVR_gl GL)
 find_library(OVR_x11 X11)
 find_library(OVR_xrandr Xrandr)
 find_library(OVR_rt rt)
 
+
 find_package_handle_standard_args(OVR DEFAULT_MSG
 	OVR_LIBRARIES
 	OVR_INCLUDE_DIR_LIB
 	OVR_INCLUDE_DIR_SRC
 	OVR_LIBRARY_SO
-	OVR_udev
 	OVR_pthread
 	OVR_gl
 	OVR_x11
@@ -55,4 +54,4 @@ find_package_handle_standard_args(OVR DEFAULT_MSG
 
 # append the two include directories together.
 set(OVR_INCLUDE_DIRS ${OVR_INCLUDE_DIR_LIB} ${OVR_INCLUDE_DIR_SRC})
-set(OVR_LIBRARIES ${OVR_LIBRARIES} ${OVR_LIBRARY_SO} ${OVR_udev} ${OVR_pthread} ${OVR_gl} ${OVR_x11} ${OVR_xrandr} ${OVR_rt})
+set(OVR_LIBRARIES ${OVR_LIBRARIES} ${OVR_LIBRARY_SO} ${OVR_pthread} ${OVR_gl} ${OVR_x11} ${OVR_xrandr} ${OVR_rt})
