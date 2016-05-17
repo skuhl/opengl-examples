6b86a6fa31e5b95a576b9ab7fa679031b3750422
Scott Kuhl <kuhl@mtu.edu>
2016-05-16 20:31:02 -0400

    fix typo in comment

--- lib/msg.c
+++ lib/msg.c
@@ -179,7 +179,7 @@ static void msg_start_color(msg_type type, FILE *stream)
 
 }
 
-/** Writes bytes to a stream to reset the colors back to he default. */
+/** Writes bytes to a stream to reset the colors back to the default. */
 static void msg_end_color(msg_type type, FILE *stream)
 {
 	if(stream == NULL || isatty(fileno(stream)) == 0)
