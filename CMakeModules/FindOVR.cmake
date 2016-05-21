1b66f26c08fa7061bff0c3e62a9a65dff67bab6f
Scott Kuhl <kuhl@mtu.edu>
2016-05-21 11:47:58 -0400

    remove old cmake code

--- CMakeModules/FindOVR.cmake
+++ CMakeModules/FindOVR.cmake
@@ -1,32 +1,5 @@
 include(FindPackageHandleStandardArgs)
 
-# Rekhi Linux lab
-#set(CMAKE_LIBRARY_PATH "/local/kuhl-public-share/opengl/ovr_sdk_linux_0.4.4/LibOVR/Lib/Linux/Release/x86_64" ${CMAKE_LIBRARY_PATH})
-#set(CMAKE_INCLUDE_PATH "/local/kuhl-public-share/opengl/ovr_sdk_linux_0.4.4/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
-#set(CMAKE_INCLUDE_PATH "/local/kuhl-public-share/opengl/ovr_sdk_linux_0.4.4/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
-set(CMAKE_LIBRARY_PATH "/local/kuhl-public-share/opengl/ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Debug" ${CMAKE_LIBRARY_PATH})
-set(CMAKE_LIBRARY_PATH "/local/kuhl-public-share/opengl/ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Release" ${CMAKE_LIBRARY_PATH})
-set(CMAKE_INCLUDE_PATH "/local/kuhl-public-share/opengl/ovr_sdk_linux_0.5.0.1/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
-set(CMAKE_INCLUDE_PATH "/local/kuhl-public-share/opengl/ovr_sdk_linux_0.5.0.1/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
-
-# Kuhl's machines
-set(CMAKE_LIBRARY_PATH "/home/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Debug" ${CMAKE_LIBRARY_PATH})
-set(CMAKE_LIBRARY_PATH "/home/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Release" ${CMAKE_LIBRARY_PATH})
-set(CMAKE_INCLUDE_PATH "/home/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
-set(CMAKE_INCLUDE_PATH "/home/kuhl/public-ogl/ovr_sdk_linux_0.5.0.1/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
-
-
-# Allow folder to be stored alongside the opengl-examples folder.
-#set(CMAKE_LIBRARY_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.4.4/LibOVR/Lib/Linux/Release/x86_64" ${CMAKE_LIBRARY_PATH})
-#set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.4.4/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
-#set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.4.4/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
-set(CMAKE_LIBRARY_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Debug" ${CMAKE_LIBRARY_PATH})
-set(CMAKE_LIBRARY_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.5.0.1/LibOVR/Lib/Linux/x86_64/Release" ${CMAKE_LIBRARY_PATH})
-set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.5.0.1/LibOVR/Include" ${CMAKE_INCLUDE_PATH})
-set(CMAKE_INCLUDE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../ovr_sdk_linux_0.5.0.1/LibOVR/Src" ${CMAKE_INCLUDE_PATH})
-
-
-
 # library was named libovr.a in 0.4.4 and libOVR.a in 0.5.0.1
 find_library(OVR_LIBRARIES libOVR.a libovr.a)
 find_library(OVR_LIBRARY_SO libOVRRT64_0.so.5)
